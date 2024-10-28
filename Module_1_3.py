d252446b45023c5bb4490e76bf2b16f8b603b627
RomanPautov <pautov156@gmail.com>
2024-10-28 13:42:41 +0700

    Add files via upload

---
+++ Module_1_3.py
@@ -0,0 +1,15 @@
+name = 'Роман'
+
+print(name)
+
+age = 21
+
+print(age)
+
+age = 21+1
+
+print(age)
+
+is_student = True
+
+print(is_student)
---
+++ переменные.py
@@ -0,0 +1,6 @@
+homework = 12
+hours = 1.5
+name = 'Python'
+time = (hours / homework)
+
+print('курс: ',name,',',' всего задач: ', homework,',' ,' затрачено часов: ', hours,',', ' среднее время выполнения ', time,'.',sep='')
